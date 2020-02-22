421495d21403aec079b34e33859c40d148da0f0f
KucingMaboek <45931684+KucingMaboek@users.noreply.github.com>
2020-02-22 19:32:38 +0700

    qqqqq

--- Assets/Script/ActionButton.cs
+++ Assets/Script/ActionButton.cs
@@ -3,28 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ActionButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class ActionButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
 {
     public PlayerControl player;
     public void OnPointerDown(PointerEventData eventData)
     {
         player.isSwitchingWeapon();
     }
-
+    
     public void OnPointerUp(PointerEventData eventData)
     {
 
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
